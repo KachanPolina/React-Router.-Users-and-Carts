053fb6745c72b07f4602b1f93a020ee95044eaa2
Polina Kachan <kachan.polina8@gmail.com>
2026-08-26 14:39:14 +0300

    Add styles to user form

--- src/components/users/UserForm/UserForm.jsx
+++ src/components/users/UserForm/UserForm.jsx
@@ -1,8 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import { schemaUserForm } from '../../../utils/formValidationShema';
 import { updateUser } from '../../../store/slices/usersSlice';
+import './UserForm.css';
+import { Button, TextField } from '@mui/material';
 
 function UserForm() {
   const dispatch = useDispatch();
@@ -10,7 +15,7 @@ function UserForm() {
   const { id } = useParams();
   const currentUser = users.find((user) => user.id === Number(id));
   const navigate = useNavigate();
-  
+
   const getInitialValues = () => {
     return {
       ...currentUser,
@@ -29,86 +34,139 @@ function UserForm() {
     dispatch(updateUser(updatedValues));
   };
 
-
   const goBack = () => navigate(-1);
 
   const renderForm = ({ isValid }) => {
     return (
-      <Form id='users-form'>
-        <div>
+      <Form id='user-form'>
+        <div className='input-container'>
           <label htmlFor='name'>Name</label>
-          <Field type='text' name='name' id='name' placeholder='name' />
+          <Field
+            type='text'
+            name='name'
+            id='name'
+            placeholder='name'
+            as={TextField}
+            className='input'
+          />
         </div>
-        <ErrorMessage name='name'>
-          {(message) => <div>{message}</div>}
-        </ErrorMessage>
-
-        <fieldset id='contact' form='users-form'>
+        <div className='error-message'>
+          <ErrorMessage name='name'>
+            {(message) => <div>{message}</div>}
+          </ErrorMessage>
+        </div>
+        <fieldset id='contact' form='users-form' className='form-fieldset'>
           <legend>Contact</legend>
-          <div>
+          <div className='input-container'>
             <label htmlFor='email'>Email</label>
-            <Field type='email' name='email' id='email' placeholder='Email' />
+            <Field
+              type='email'
+              name='email'
+              id='email'
+              placeholder='Email'
+              as={TextField}
+              className='input'
+            />
+          </div>
+          <div className='error-message'>
             <ErrorMessage name='email'>
               {(message) => <div>{message}</div>}
             </ErrorMessage>
           </div>
-          <div>
+          <div className='input-container'>
             <label htmlFor='phone'>Phone</label>
-            <Field type='text' name='phone' id='phone' placeholder='Phone' />
+            <Field
+              type='text'
+              name='phone'
+              id='phone'
+              placeholder='Phone'
+              as={TextField}
+              className='input'
+            />
+          </div>
+          <div className='error-message'>
             <ErrorMessage name='phone'>
               {(message) => <div>{message}</div>}
             </ErrorMessage>
           </div>
         </fieldset>
-        <fieldset id='contact' form='users-form'>
+        <fieldset id='contact' form='users-form' className='form-fieldset'>
           <legend>Address</legend>
-          <div>
+          <div className='input-container'>
             <label htmlFor='city'>City</label>
             <Field
               type='text'
               name='address.city'
               id='city'
               placeholder='City'
+              as={TextField}
+              className='input'
             />
           </div>
-          <div>
+          <div className='input-container'>
             <label htmlFor='state'>State</label>
             <Field
               type='text'
               name='address.state'
               id='state'
               placeholder='State'
+              as={TextField}
+              className='input'
             />
           </div>
-          <div>
+          <div className='input-container'>
             <label htmlFor='address'>Address</label>
             <Field
               type='text'
               name='address.address'
               id='address'
               placeholder='Address'
+              as={TextField}
+              className='input'
             />
           </div>
         </fieldset>
-        <button type='submit' disabled={!isValid}>
-          Save
-        </button>
-        <button type='reset'>Reset</button>
-        <button type='button' onClick={goBack}>
-          Return
-        </button>
+        <div className='buttons-container'>
+          <Button
+            type='submit'
+            disabled={!isValid}
+            variant='contained'
+            endIcon={<SaveAltIcon />}
+            color='success'
+          >
+            Save
+          </Button>
+          <Button
+            type='reset'
+            variant='contained'
+            endIcon={<RestartAltIcon />}
+            color='error'
+          >
+            Reset
+          </Button>
+          <Button
+            type='button'
+            onClick={goBack}
+            variant='contained'
+            endIcon={<KeyboardReturnIcon />}
+          >
+            Return
+          </Button>
+        </div>
       </Form>
     );
   };
 
   return (
-    <Formik
-      initialValues={getInitialValues()}
-      onSubmit={onFormSubmit}
-      validationSchema={schemaUserForm}
-    >
-      {renderForm}
-    </Formik>
+    <div className='main-form-container'>
+      <Formik
+        initialValues={getInitialValues()}
+        onSubmit={onFormSubmit}
+        validationSchema={schemaUserForm}
+      >
+        {renderForm}
+      </Formik>
+    </div>
   );
 }
 
--- src/components/users/UsersProducts/UsersProducts.jsx
+++ src/components/users/UsersProducts/UsersProducts.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getUsersProducts } from '../../../store/slices/productsSlice';
+import '../../carts/CartsProducts/CartsProducts.css'
 
 function UsersProducts() {
   const dispatch = useDispatch();
@@ -13,16 +14,22 @@ function UsersProducts() {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <ul className='carts-products'>
       {products.map(({ id, title, price, quantity, thumbnail }) => {
         return (
-          <li key={id}>
+          <li key={id} className='product-cart'>
             <img src={thumbnail} alt={title} />
-            {title} | Price - {price} | Quantity - {quantity}
+            <div className='product-info'>
+              <h3>{title} </h3>
+              <div className='price-and-quantity'>
+                <p>Price - {price}</p>
+                <p>Quantity - {quantity}</p>
+              </div>
+            </div>
           </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
 
