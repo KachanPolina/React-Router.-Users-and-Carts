cee698b09bcfd2091254e64d9cd9d18c14a54501
Polina Kachan <kachan.polina8@gmail.com>
2026-08-26 13:01:36 +0300

    Add styles to carts products

--- src/components/carts/CartsProducts/CartsProducts.jsx
+++ src/components/carts/CartsProducts/CartsProducts.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getCartsProducts } from '../../../store/slices/productsSlice';
+import './CartsProducts.css';
 
 function CartsProducts() {
   const dispatch = useDispatch();
@@ -13,16 +14,22 @@ function CartsProducts() {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <ul className='carts-products'>
       {products.map(({ id, title, price, quantity, thumbnail }) => {
         return (
-          <li key={id}>
+          <li key={id} className='product-cart'>
             <img src={thumbnail} alt={title} />
-            {title} | Price - {price} | Quantity - {quantity}
+            <div className='product-info'>
+              <h3>{title} </h3>
+              <div className='price-and-quantity'>
+                <p>Price - {price}</p>
+                <p>Quantity - {quantity}</p>
+              </div>
+            </div>
           </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
 
